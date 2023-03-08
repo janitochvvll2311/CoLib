6d8f2fed5fb8a5b58938ac94c0357e3abcddcfd4
CHAVA <janitochvvll2311@gmail.com>
2023-03-08 11:11:44 -0600

    Primitive type aliases added

--- tst/main.cpp
+++ tst/main.cpp
@@ -3,5 +3,9 @@
 int main()
 {
     std::cout << "It Works\n";
+    std::cout << sizeof(float);
+    std::cout << sizeof(double);
+    std::cout << sizeof(long double);
+    std::cin.get();
     return 0;
 }
