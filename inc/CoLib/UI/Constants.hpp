cd58b19b40f37918f87794a31cf980e2a9b0d165
CHAVA <janitochvvll2311@gmail.com>
2023-03-20 20:43:01 -0600

    Node interfaces added

---
+++ inc/CoLib/UI/Constants.hpp
@@ -0,0 +1,17 @@
+#ifndef COLIB_UI_CONSTANTS_HPP
+#define COLIB_UI_CONSTANTS_HPP
+
+#include <CoLib/UI/Export.hpp>
+
+namespace co
+{
+
+    const extern s8t COLIB_UI_API NODE_ALREADY_ATTACHED_STRING;
+    const extern s8t COLIB_UI_API NODE_ALREADY_DETACHED_STRING;
+    const extern s8t COLIB_UI_API OWN_ANCESTOR_NODE_STRING;
+    const extern s8t COLIB_UI_API PARENT_NODES_NOT_SUPPORTED_STRING;
+    const extern s8t COLIB_UI_API CHILD_NODES_NOT_SUPPORTED_STRING;
+
+}
+
+#endif // COLIB_UI_CONSTANTS_HPP
